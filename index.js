3b064fbce0c0a7e61d3f569c6226b20d6c631b32
brayaneider1 <b.alvarez@udla.edu.co>
2021-11-12 01:27:04 -0500

    feat(index):add api

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ const app = express()
 const stripe = new Stripe('sk_test_51JsKcvG0L7Mcv8rUfCRxplsvPa8QusifVjnMKwLWCCeZllyQTGXzFB4axP4SnXw7FNvpfMY0j96bdNLixVcGEKag00k78K431L')
 
 app.use(cors({ origin: 'http://localhost:3000' }))
-app.use(express.json)
+app.use(express.json())
 
-app.post('/api/checkout', (req, res) => {
-  const { id, amount } = req.body
-  await stripe.paymentIntents.create({
-    amount,
-    currency: 'USD',
-    description: 'Subscription to Tesigue',
-    payment_method: id,
-    confirm: true
-  })
-  res.send({ message: 'Sucesfull payment' })
+app.post('/api/checkout', async (req, res) => {
+  try {
+    const { id, amount } = req.body
+    await stripe.paymentIntents.create({
+      amount,
+      currency: 'USD',
+      description: 'Subscription to Tesigue',
+      payment_method: id,
+      confirm: true
+    })
+    res.send({ message: 'Sucesfull payment' })
+  } catch (error) {
+    res.json({ message: error.raw.message })
+  }
+
+})
+
+app.get('/', async (req, res) => {
+  res.send('Hello motherfuckers')
 })
 
 app.listen(4000, () => {
