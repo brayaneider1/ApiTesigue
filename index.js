7c17c6eceafbcfc3a4f8d52e2042da149f7d0f6d
brayaneider1 <b.alvarez@udla.edu.co>
2021-11-12 00:25:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const Stripe = require('stripe')
+const cors = require('cors')
+
+const app = express()
+
+const stripe = new Stripe('sk_test_51JsKcvG0L7Mcv8rUfCRxplsvPa8QusifVjnMKwLWCCeZllyQTGXzFB4axP4SnXw7FNvpfMY0j96bdNLixVcGEKag00k78K431L')
+
+app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(express.json)
+
+app.post('/api/checkout', (req, res) => {
+  const { id, amount } = req.body
+  await stripe.paymentIntents.create({
+    amount,
+    currency: 'USD',
+    description: 'Subscription to Tesigue',
+    payment_method: id,
+    confirm: true
+  })
+  res.send({ message: 'Sucesfull payment' })
+})
+
+app.listen(4000, () => {
+  console.log('server on port ', 4000);
+})
+
